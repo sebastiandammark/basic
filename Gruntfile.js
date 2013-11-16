3b0d22e61403513c6701b5cf02e1ecffe09c58e2
Sebastian Dammark <sebastian@sheriffone.com>
2013-11-16 15:19:18 +0100

    New grunt setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,161 +1,122 @@
-/*global module:false*/
 module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    concat: {
-			dev: {
-        src: ['app/js/*.js'],
-        dest: 'public/js/functions.min.js'
-      },
-			dist: {
-        src: ['app/js/*.js'],
-        dest: 'tmp/js/functions.min.js'
-      }
-    },
-    uglify: {
-      dist: {
-        src: '<%= concat.dist.dest %>',
-        dest: 'public/js/functions.min.js'
-      }
-    },
-    jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        unused: true,
-        boss: true,
-        eqnull: true,
-        browser: true,
-        globals: {}
-      },
-      gruntfile: {
-        src: 'Gruntfile.js'
-      },
-    },
-		compass: {
-	    dist: {
-	      options: {
-					httpPath: 'public',
-	        sassDir: 'app/sass',
-					imagesDir: 'public/images',
-	        cssDir: 'public/css',
-	        environment: 'production',
-					outputStyle: 'compressed',
-					relativeAssets: true,
-					noLineComments: true,
-					force: true
-	      },
-	    },
-	    dev: {
-	      options: {
-					httpPath: 'public',
-	        sassDir: 'app/sass',
-					imagesDir: 'public/images',
-	        cssDir: 'public/css',
-					environment: 'development',
-					outputStyle: 'expanded',
-					relativeAssets: true,
-					noLineComments: false
-	      },
-	    }
-	  },
-		clean: ["tmp"],
+    pkg: grunt.file.readJSON('package.json'),
+		connect: {
+			server: {
+				options: {
+					port: 9000,
+					base: 'public'
+				}
+			}
+		},
+		sass: {
+		dev: {
+			options: {
+				style: 'expanded'
+			},
+			files: {
+				'tmp/css/concat/styles.css': 'app/sass/styles.scss'
+			}
+		},
+		dist: {
+			options: {
+				style: 'compressed'
+			},
+			files: {
+				'tmp/css/concat/styles.css': 'app/sass/styles.scss'
+			}
+		}
+	},
+  	concat: {
+			js: {
+        src: ['app/scripts/**js', 'app/vendor/**/*.js'],
+        dest: 'public/js/app.js'
+			},
+			css: {
+      	src: ['tmp/css/concat/styles.css', 'app/vendor/**/*.css'],
+        dest: 'tmp/css/prefix/styles.css'
+			}
+		},
+		autoprefixer: {
+			css: {
+				options: {
+					browsers: ['last 2 version', 'ie 8', 'ie 7']
+				},
+				src: 'tmp/css/prefix/styles.css',
+				dest: 'public/css/styles.css'
+			}
+		},
+		copy: {
+	  	main: {
+	    	files: [
+	      	{expand: true, flatten: true, src: ['app/vendor/**/fonts/*.*'], dest: 'public/fonts/'},
+	      	{expand: true, flatten: true, src: ['app/vendor/**/*.{png,jpg,gif}'], dest: 'tmp/images/vendor/'},
+					{expand: true, flatten: true, src: ['app/markup/*.html'], dest: 'public/'}
+	    	]
+	  	},
+	  	dist: {
+	    	files: [
+					{expand: true, flatten: false, cwd: 'public/', src: ['**/*'], dest: 'dist/'}
+	    	]
+	  	}
+		},
 		imagemin: {
-	    png: {
-	      options: {
-	        optimizationLevel: 7
-	      },
-	      files: [
-	        {
-	          // Set to true to enable the following options…
-	          expand: true,
-	          // cwd is 'current working directory'
-	          cwd: 'app/assets/images/',
-	          src: ['**/*.png'],
-	          // Could also match cwd line above. i.e. project-directory/img/
-	          dest: 'public/images',
-	          ext: '.png'
-	        }
-	      ]
-	    },
-	    jpg: {
-	      options: {
-	        progressive: true
-	      },
-	      files: [
-	        {
-	          // Set to true to enable the following options…
-	          expand: true,
-	          // cwd is 'current working directory'
-	          cwd: 'app/assets/images/',
-	          src: ['**/*.jpg'],
-	          // Could also match cwd. i.e. project-directory/img/
-	          dest: 'public/images',
-	          ext: '.jpg'
-	        }
-	      ]
-	    },
-			gif: {
-	      options: {
-	        progressive: true
-	      },
-	      files: [
-	        {
-	          // Set to true to enable the following options…
-	          expand: true,
-	          // cwd is 'current working directory'
-	          cwd: 'app/assets/images/',
-	          src: ['**/*.gif'],
-	          // Could also match cwd. i.e. project-directory/img/
-	          dest: 'public/images',
-	          ext: '.gif'
-	        }
-	      ]
-	    }
-	  },
-		'ftp-deploy': {
-		  stage: {
-		    auth: {
-		      host: '127.0.0.1',
-		      port: 21,
-		      authKey: 'figureitout'
-		    },
-		    src: 'public',
-		    dest: '{destination path}',
-		    exclusions: ['public/**/.DS_Store', 'public/**/Thumbs.db'],
-		    server_sep: '/'
-		  }
+			vendor: {
+				files: [{
+					expand: true,
+					cwd: 'tmp/images/vendor/',
+					src: ['**/*.{png,jpg,gif}'],
+					dest: 'public/images/vendor/'
+				}]
+			},
+			images: {
+				files: [{
+					expand: true,
+					cwd: 'app/images/',
+					src: ['**/*.{png,jpg,gif}'],
+					dest: 'public/images/'
+				}]
+			}
+  	},
+		min: {
+		    dist: {
+		        src: ['public/js/app.js'],
+		        dest: 'dist/js/app.js'
+		    }
 		},
+		cssmin: {
+		    dist: {
+		        src: ['public/css/styles.css'],
+		        dest: 'dist/css/styles.css'
+		    }
+		},
+		clean: ['tmp'],
 		watch: {
-		  css: {
-		    files: ['app/**/*.scss', 'app/**/*.js'],
-		    tasks: ['compass:dev', 'concat:dev'],
-		    options: {
-		      livereload: true,
-		    },
-		  },
-		},
+			main: {
+	    	files: ['app/**/*.{html,js,scss}'],
+	    	tasks: ['build']
+	  	}
+		}
   });
 
-  // These plugins provide necessary tasks.
+  // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-clean');
-	grunt.loadNpmTasks('grunt-contrib-imagemin');
-	grunt.loadNpmTasks('grunt-ftp-deploy');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-ftp-deploy');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-yui-compressor');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
-  // Default task.
-  grunt.registerTask('default', ['watch', 'concat:dev', 'compass:dev', 'clean', 'imagemin']);
-  grunt.registerTask('build', ['concat:dist', 'uglify:dist', 'compass:dist', 'clean', 'imagemin']);
-  grunt.registerTask('deploy:stage', ['build', 'ftp-deploy:stage']);
+  // Default task(s).
+  grunt.registerTask('build', ['sass:dev', 'concat', 'autoprefixer', 'copy:main', 'imagemin', 'clean']);
+  grunt.registerTask('dist', ['build', 'copy:dist', 'min', 'cssmin', 'clean']);
+  grunt.registerTask('server', ['connect', 'build', 'watch']);
+
 };
